f4ba47343ee24eaef8441b1e8cb6ec17090c003f
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-15 15:40:51 +0530

    feat(hough): added hough transform to identify lanes

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -73,3 +73,48 @@ blur_gray = cv2.GaussianBlur(gray_img, (kernel_size, kernel_size), 0)
 low_threshold = 100
 high_threshold = 180
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Pasted Code
+# Next we'll create a masked edges image using cv2.fillPoly()
+mask = np.zeros_like(edges)   
+ignore_mask_color = 255   
+
+# This time we are defining a four sided polygon to mask
+vertices = np.array([[(0,x_size),(0, 40), (160, 40), (y_size,x_size)]], dtype=np.int32)
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(edges, mask)
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1 # distance resolution in pixels of the Hough grid
+theta = np.pi/180 # angular resolution in radians of the Hough grid
+threshold = 2     # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 4 #minimum number of pixels making up a line
+max_line_gap = 5    # maximum gap in pixels between connectable line segments
+line_image = np.copy(image)*0 # creating a blank to draw lines on
+
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+# Iterate over the output "lines" and draw lines on a blank image
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
+
+# Create a "color" binary image to combine with line image
+color_edges = np.dstack((edges, edges, edges)) 
+
+# Draw the lines on the edge image
+lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
+lines_edges = cv2.polylines(lines_edges,vertices, True, (0,0,255), 3)
+
+plt.imshow(edges, cmap="Greys_r")
+plt.show()
+plt.imshow(image)
+plt.title("Input Image")
+plt.show()
+plt.imshow(lines_edges)
+plt.title("Colored Lane line [In RED] and Region of Interest [In Blue]")
+plt.show()
