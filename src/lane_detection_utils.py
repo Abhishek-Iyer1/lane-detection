5ea8b0357d880b2c0b95b5b0ea1c1d4221384867
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-18 15:51:40 +0530

    feat(masking): added masking to the pipeline

--- src/lane_detection_utils.py
+++ src/lane_detection_utils.py
@@ -17,3 +17,28 @@ def canny(image, low_thresh, high_thresh):
     print(f"Applying Canny Edge Detection...")
     return cv2.Canny(image, low_thresh, high_thresh)
 
+def apply_mask(image, vertices):
+    """
+    Applies and returns image mask defined by 'vertices'.
+
+    Pixels outside the vertices are set to black. 
+    """
+    print(f"Masking region of interest...")
+    # Defining mask color based on n_channels in image
+    if len(image.shape) > 2:
+        n_channels = image.shape[2]
+        mask_color = (255,) * n_channels
+    else:
+        mask_color = 255
+
+    # Initializing blank mask
+    mask = np.zeros_like(image)   
+        
+    # Fills pixels within the vertices with given color    
+    cv2.fillPoly(mask, vertices, mask_color)
+    
+    # Applies mask on the image in a bitwise 'and' operation
+    image_with_mask = cv2.bitwise_and(image, mask)
+    return image_with_mask
+
+
