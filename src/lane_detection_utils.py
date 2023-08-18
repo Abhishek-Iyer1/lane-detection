71893b44f6ddd0a3fa7e88e281c8e22244df9503
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-18 15:50:47 +0530

    feat(canny): added canny edge detection to the pipeline

--- src/lane_detection_utils.py
+++ src/lane_detection_utils.py
@@ -11,3 +11,9 @@ def gaussian_blur(image, kernel_size):
     """Applies a Gaussian Noise kernel"""
     print(f"Applying Gaussian Blur...")
     return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+def canny(image, low_thresh, high_thresh):
+    """Applies the Canny transform"""
+    print(f"Applying Canny Edge Detection...")
+    return cv2.Canny(image, low_thresh, high_thresh)
+
