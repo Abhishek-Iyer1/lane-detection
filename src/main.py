50c3a184884c79da9af15eec106a88a7760192be
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-26 23:48:15 +0530

    (training): added method for training unet model

--- src/main.py
+++ src/main.py
@@ -19,19 +19,56 @@ def main():
     images = lane_detection_pipeline_opencv(image)
 
     # Add the ground truth image to the list of images
-    images.append(("Test Label", test[int(sys.argv[1])]))
+    images.append(("Test Label", labels[int(sys.argv[1])]))
 
-    # Plot results
-    fig = plt.figure()
+    # # Plot results
+    # fig = plt.figure()
 
-    for i, image_data in list(zip(range(1,len(images)), images)):
-        title, image = image_data
-        fig.add_subplot(1, len(images), i)
-        plt.title(title)
-        plt.axis("off")
-        plt.imshow(image)
+    # for i, image_data in list(zip(range(1,len(images)), images)):
+    #     title, image = image_data
+    #     fig.add_subplot(1, len(images), i)
+    #     plt.title(title)
+    #     plt.axis("off")
+    #     plt.imshow(image)
 
-    plt.show()
+    # plt.show()
+
+    my_unet = UNET(input_shape=image.shape, trainable=True, start_filters=16, name="trial unet")
+    my_unet.model.summary()
+    train_model(my_unet, x_data=train, y_data=labels)
+    # image = train[5]
+    # image = np.reshape(image, [1, 80, 160, 3])
+    # print(f"Image Shape: {image.shape}")
+    # output = my_unet.model.predict(image)
+    # print(f"Output: {output}")
+    # output = np.reshape(output, (80, 160, 1))
+    # plt.imshow(output)
+    # plt.show()
+
+def train_model(model: UNET, x_data: list, y_data: list):
+    
+    test_output = train_test_split(np.array(x_data).reshape(-1, 80, 160, 3), np.array(y_data).reshape(-1, 80, 160, 1), train_size=0.8, test_size=0.2)
+    # x_train = 
+    # y_train = 
+    # validation_data = 
+
+    early_stopping = EarlyStopping(patience=10, verbose=1)
+    model_checkpoint = ModelCheckpoint("./keras.model", save_best_only=True, verbose=1)
+    reduce_lr = ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1)
+    
+    batch_size = 32
+    epochs = 200
+    callbacks = [early_stopping, model_checkpoint, reduce_lr]
+
+    model.train_model(x_train=x_train, 
+                      y_train=y_train, 
+                      validation_data=validation_data, 
+                      batch_size=batch_size, 
+                      epochs=epochs, 
+                      callbacks=callbacks, 
+                      optimizer="adam", 
+                      loss="binary_cross_entropy", 
+                      metrics=["accuracy", "loss"])
 
 # Lane finding Pipeline
 def lane_detection_pipeline_opencv(image):
