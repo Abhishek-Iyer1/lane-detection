4e9d5d7ceba22ff0c56bc082216b401adf61ed4c
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-29 22:21:22 +0530

    fix(structuring): deleted classical pipeline code and comments from the deep learning pipeline in order for better readability

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import pickle
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from lane_detection_utils import *
+from src.classical_utils import *
 from unet import UNET
 from keras import Model
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
@@ -31,9 +31,6 @@ def main():
     train = np.array(pickle.load(open("data/full_CNN_train.p", 'rb')))
     labels = np.array(pickle.load(open("data/full_CNN_labels.p", 'rb')))
 
-    # # Find image number as given through cli from train data
-    # image = train[int(sys.argv[1])]
-
     # Train on subset of the dataset due to memory limitations
     train = train[0:6000]
     labels = labels[0:6000]
@@ -44,24 +41,6 @@ def main():
 
     # Generates train and test datasets from the original dataset after shuffling, splitting, and creating batches.
     train_dataset, test_dataset = prepare_dataset(train, labels, batch_size=32)
-
-    # # Run lane detection pipeline on the image
-    # images = lane_detection_pipeline_opencv(image)
-
-    # # Add the ground truth image to the list of images
-    # images.append(("Test Label", labels[int(sys.argv[1])]))
-
-    # # Plot results
-    # fig = plt.figure()
-
-    # for i, image_data in list(zip(range(1,len(images)), images)):
-    #     title, image = image_data
-    #     fig.add_subplot(1, len(images), i)
-    #     plt.title(title)
-    #     plt.axis("off")
-    #     plt.imshow(image)
-
-    # plt.show()
 
     # Training
     # my_unet = UNET(input_shape=image.shape, trainable=True, start_filters=16, name="trial unet")
@@ -142,21 +121,6 @@ def train_model(model: UNET, train_dataset: tf.data.Dataset, test_dataset: tf.da
         return history
     else:
         raise ValueError (f"smodel.trainable value is {model.trainable}. Please set the value to True in order to train the model.")
-
-# Lane finding Pipeline
-def lane_detection_pipeline_opencv(image):
-    """
-    Description: 
-        The whole pipeline for detecting lanes using classical approaches with opencv.
-    """
-    gray_image = grayscale(image)
-    smoothed_image = gaussian_blur(image = gray_image, kernel_size = 5)
-    canny_image = canny(image = smoothed_image, low_thresh = 180, high_thresh = 240)
-    masked_image = apply_mask(image = canny_image, vertices = get_vertices(image))
-    houghed_line_image = hough_lines(image = masked_image, rho = 1, theta = np.pi/180, thresh = 2, min_line_len = 4, max_line_gap = 5)
-    final_output = weighted_img(line_image = houghed_line_image, initial_image=image, vertices= get_vertices(image), alpha=0.8, beta=1, gamma=0)
-    
-    return [("Gray Image", gray_image), ("Smoothed Image", smoothed_image), ("Canny Image", canny_image), ("Masked Image", masked_image), ("Hough Transform Image", houghed_line_image), ("Final Ouput", final_output)]
 
 if __name__ == '__main__':
     main()
