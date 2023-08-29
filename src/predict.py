689f73e95b68475df7fdd74a49c0be82463c19e4
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-08-29 23:19:54 +0530

    feat(iou): added a function to calculate iou for our predicted image vs ground truth image

--- src/predict.py
+++ src/predict.py
@@ -12,21 +12,28 @@ def predict_lanes():
     labels = np.array(pickle.load(open("data/full_CNN_labels.p", 'rb')))
 
     # Find image number as given through cli from train data
-    image = train[int(sys.argv[1])]
-    ground_truth = labels[int(sys.argv[1])]
+    image = np.array(train[int(sys.argv[1])]) / 255
+    ground_truth = np.array(labels[int(sys.argv[1])]) / 255
 
     # Load saved model
     my_unet: Model = tf.keras.models.load_model('keras.model')
 
     # Generate predictions
     image_reshaped = np.reshape(image, [1, 80, 160, 3])
-    output_test = my_unet.predict(image_reshaped)
-    output = np.reshape(output_test, (80, 160, 1))
+    prediction = my_unet.predict(image_reshaped)
+    prediction = np.reshape(prediction, (80, 160, 1))
+
+    # Binarize values to calculat IoU
+    prediction = prediction.round()
+    ground_truth = ground_truth.round()
+
+    # Calculat IoU
+    iou = calculate_iou(ground_truth, prediction)
 
     images = [
         (f"Train Example, image no. {sys.argv[1]}", image),
         (f"Ground Truth, image no. {sys.argv[1]}", ground_truth),
-        (f"Predicted Output", output)
+        (f"Predicted Output, IoU: {iou:.2f}", prediction)
     ]
 
     # Plot results
@@ -40,6 +47,28 @@ def predict_lanes():
         plt.imshow(image)
 
     plt.show()
+
+def calculate_iou(y_true, y_pred):
+    # Calculate the true positives,
+    # false positives, and false negatives
+    tp = 0
+    fp = 0
+    fn = 0
+
+    for i in range(len(y_true)):
+        for j in range(len(y_true[0])):
+            if y_true[i][j] == 1 and y_pred[i][j] == 1:
+                tp += 1
+            elif y_true[i][j] == 0 and y_pred[i][j] == 1:
+                fp += 1
+            elif y_true[i][j] == 1 and y_pred[i][j] == 0:
+                fn += 1
+
+    # Calculate IoU
+    iou = tp / (tp + fp + fn)
+
+    return iou
+ 
 
 if __name__ == '__main__':
     predict_lanes()
